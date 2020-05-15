67e2830302fb34c2a32dfb92f1f9201446777dea
abhirahul96 <abhi.rahulnayak@gmail.com>
2020-05-16 02:41:46 +0530

    new css

--- todo.js
+++ todo.js
@@ -1,16 +1,17 @@
 document.getElementById("Add").addEventListener("click",newobj)
 
 function newobj(){
-    var newtd=document.querySelector('.newtodo').value
+    var newtd=document.querySelector('.newtodo')
     var ol=document.getElementById('ordlist')
     var li=document.createElement("li")
     var rb=document.createElement("input")
     rb.setAttribute("type","checkbox")
     rb.setAttribute("labels",newtd)
-    li.textContent=newtd
+    li.textContent=newtd.value
     li.appendChild(rb)
     ol.appendChild(li)
     rb.addEventListener("click",function(){
         li.classList.toggle('strike')
     })
+    newtd.value=""
 }
